ca4b4c64b81590d93863c5eaff3c6cccb6f2f630
romainpujol <romain.pujol@ensta-paris.fr>
2024-12-20 16:27:03 +0100

    update

--- projet.py
+++ projet.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal, norm
 np.random.seed(1)
 
 def f(t,phi):
@@ -56,8 +57,104 @@ def viral_load_to_censored(viral_load_,loq=2.6):
 
     return index
     
+def p(phi,i,y,mu_current,omega_current,sigma_current,table_t=np.array([1,3,7,14,28,56])):
+    #y represents the whole matrix (with imputed values of ycensored)
+    #i represents the line we use
+    N,K = y.shape
+    value = 0
+    if omega_current == 0:
+        print("error, omega = 0")
+    else:
+        matrix=(1/omega_current)*np.eye(4)
+        value -=(phi-mu_current).T @ matrix @ (phi[i,:]-mu_current)/2
+        for j in range(K):
+            value -= (1/(2*sigma_current))*(y[i,j]-f(table_t[j],phi))**2
+
+    return np.exp(value)
+
+def proposition_prior(current_mu,current_omega,current_phi,current_sigma,current_y):
+    #current_mu, current_omega are two parameters that are being constructed in the procedure
+    N,K = current_phi.shape
+    update = np.zeros((N,K))
+    for i in range(N):
+        proposition_=np.random.multivariate_normal(current_mu,current_omega*np.eye(4))
+        q_ratio = multivariate_normal.pdf(proposition_,current_mu,current_omega*np.eye(4))/multivariate_normal.pdf(current_phi[i,:],current_mu,current_omega*np.eye(4))
+        acc_rate = np.minimum(1,q_ratio*p(proposition_,i,current_y,current_mu,current_omega,current_sigma)/p(current_phi[i,:],i,current_y,current_mu,current_omega,current_sigma))
+        u = np.random.rand()
+        if u<acc_rate:
+            update[i,:]=proposition_
+
+    return update
+
+def proposition_multidim_rw(current_phi,current_omega,current_sigma,current_y,current_mu,lambd=1):
+    #multidimensional random walk proposal
+    N,K = current_phi.shape
+    proposition = np.zeros((N,K))
+    density = 1
+    for i in range(N):
+        proposition_=np.random.multivariate_normal(current_phi[i,:],lambd*current_omega*np.eye(4))
+        acc_rate = np.minimum(1,p(proposition_,i,current_y,current_mu,current_omega,current_sigma)/p(current_phi,i,current_y,current_mu,current_omega,current_sigma))
+        u = np.random.rand()
+        if u<acc_rate:
+            proposition[i,:]=proposition_
+    return proposition
+
+def proposition_unidim_rw(current_phi,lambd,current_y,current_mu,current_omega,current_sigma):
+    #unidimensional random walk proposal
+    N,K = current_phi.shape
+    proposition = np.copy(current_phi)
+    for i in range(N):
+        line_i = np.copy(current_phi[i,:])
+        for j in range(K):
+            step = np.random.normal(0,lambd[j])
+            proposition[i,j]+=step
+            acc_rate = np.minimum(1,p(proposition[i,:],i,current_y,current_mu,current_omega,current_sigma)/p(line_i,i,current_y,current_mu,current_omega,current_sigma))
+            u = np.random.rand()
+            if u>acc_rate:
+                proposition[i,j]=line_i[j]
+    
+    return proposition
+
+
+def hm_algorithm_update(current_y,current_phi,current_omega,current_mu,current_sigma,lambd,method):
+    if method == 1:
+        update= proposition_prior(current_mu,current_omega,current_phi,current_sigma,current_y)
+    elif method ==2:
+        update=proposition_multidim_rw(current_phi,current_omega,current_sigma,current_y,current_mu,lambd=1)
+    elif method ==3:
+        update=proposition_unidim_rw(current_phi,lambd,current_y,current_mu,current_omega,current_sigma)
+    return update    
+    
+
+
+def update_y_cens(phi,t,sigma,LOQ):
+    m=f(t,phi) 
+    C=(f(t,phi)-LOQ)/sigma
+    alpha = (C+np.sqrt(C**2+4))/2
+    u = np.random.rand()
+    x = -1/alpha*np.log(1-u)+C
+    proba = np.exp(-(x-alpha)**2/2)
+    u = np.random.rand()
+    while u > proba :
+        u = np.random.rand()
+        x = -1/alpha * np.log(1 - u) + C
+        proba = np.exp(-(x-alpha)**2/2)
+        u = np.random.rand()
+    y = m - sigma*x
+    return y
+
+            
+def S(y,phi):
+
+
+def SAEM(y_0,)
+
+
 #à faire, calculer loi de phi^m sachant y obs, y censuré (l'estimation m-1) et avec les paramètres theta^(m-1)
 #simuler avec MH grâce à la loi trouvée (pour le calcul de l'acceptation rejet), les lois de proposition
 #sont dans l'article
 #ensuite calculer y cens(m) à partir de y obs, phi(m) et theta (m-1) (fin de la page 1566 et début 1567)
 #ensuite update theta(m-1) en theta (m) et l'algo est bouclé
+
+
+#travailler sur l'acceptance rate de proposition_multidim_rw et proposition_unidim_rw
